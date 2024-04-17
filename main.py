58cf1fa2a34c0bcb4035d0c6dcb36ac5fcdf40ef
priyanshub18 <priyanshubhardwaj658@gmail.com>
2024-04-17 18:03:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import pyttsx3
+import datetime
+import speech_recognition 
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+rate = engine.setProperty("rate",170)
+
+def speak(audio):
+    engine.say(audio) 
+    engine.runAndWait()
+
+def takeCommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        audio = r.listen(source,0,4)
+
+    try:
+        print("Understanding..")
+        query  = r.recognize_google(audio,language='en-in')
+        print(f"You Said: {query}\n")
+    except Exception as e:
+        print("Say that again")
+        return "None"
+    return query
+    
+if __name__ == "__main__":
+    while True:
+        query = takeCommand().lower()
+        if "wake up" in query:
+            from GreetMe import greetMe
+            greetMe()
+
+            while True:
+                query = takeCommand().lower()
+                if "go to sleep" in query:
+                    speak("Ok sir , You can call me anytime")
+                    break
+                elif "hello" in query:
+                    speak("Hello sir, how are you ?")
+                elif "i am fine" in query:
+                    speak("that's great, sir")
+                elif "how are you" in query:
+                    speak("Perfect, sir")
+                elif "thank you" in query:
+                    speak("you are welcome, sir")
+                elif "google" in query:
+                    from search import searchGoogle
+                    searchGoogle(query)
+                elif "youtube" in query:
+                    from search import searchYoutube
+                    searchYoutube(query)
+                elif "wikipedia" in query:
+                    from search import searchWikipedia
+                    searchWikipedia(query)
+                elif "naveen" in query:
+                    from search import searchNaveen
+                    searchNaveen(query)
+                elif "finally sleep" in query:
+                    speak("Going to sleep,sir")
+                    exit()
+                elif "the time" in query:
+                    strTime = datetime.datetime.now().strftime("%H:%M")    
+                    speak(f"Sir, the time is {strTime}")
+                elif "please remember" in query:
+                    rememberMessage = query.replace("please remember", "").replace("nova", "").strip() 
+                    remember = open("Remember.txt", "a")
+                    remember.write(rememberMessage)
+                    remember.write("\n")  
+                    remember.close()
+
+                elif "what do you remember" in query:
+                    remember = open("Remember.txt","r")
+                    speak("You told me to remember that" + remember.read())
+                elif "ipl score" in query:
+                    from plyer import notification  
+                    import requests 
+                    from bs4 import BeautifulSoup 
+                    url = "https://www.cricbuzz.com/"
+                    page = requests.get(url)
+                    soup = BeautifulSoup(page.text,"html.parser")
+                    team1 = soup.find_all(class_ = "cb-col-50 cb-ovr-flo cb-hmscg-tm-name")[0].get_text()
+                    team2 = soup.find_all(class_ = "cb-col-50 cb-ovr-flo cb-hmscg-tm-name")[1].get_text()
+                    team1_score = soup.find_all(class_ = "cb-ovr-flo")[8].get_text()
+                    team2_score = soup.find_all(class_ = "cb-ovr-flo")[10].get_text()
+
+                    a = print(f"{team1} : {team1_score}")
+                    b = print(f"{team2} : {team2_score}")
+                    
+                    notification.notify(
+                        title = "IPL SCORE :- ",
+                        message = f"{team1} : {team1_score}\n {team2} : {team2_score}",
+                        timeout = 15
+                    )
+                
